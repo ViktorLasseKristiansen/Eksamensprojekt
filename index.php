eebeb52bcea75bec03c4ae6701e76451661c54ed
Mayenco <haakansson.faxholm@gmail.com>
2019-03-20 10:35:15 +0100

    n
    Har ændret sådan, at den indlæser script for login, hvis den ikke har ens unikke brugerid

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php require("includes/header.php");?>
+
+<?php
+if (!isset($_SESSION['user_id'])) {
+  echo "<script>document.location.href='./login.php'</script>";
+}
+?>
       <div class="wrapper">
           <h1>Forside</h1>
           <?php
