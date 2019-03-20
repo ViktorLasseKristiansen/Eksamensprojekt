0522096dc3209b252852faddfc8bf129c3a47d58
Mayenco <haakansson.faxholm@gmail.com>
2019-03-20 11:14:01 +0100

    Tilføjet emner
    Har tilføjet navbar og dens css

---
+++ dropdown_nav.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="dk" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="style_nav.css">
+  </head>
+  <body>
+
+<nav>
+  <p>Navigation</p>
+  <ul>
+    <li><a href="#">Personer</a>
+      <ul>
+        <li><a href="#">Person 1</a></li>
+        <li><a href="#">Person 2</a></li>
+        <li><a href="#">Person 3</a></li>
+        <li><a href="#">Person 4</a></li>
+      </ul>
+    </li>
+    <li><a href="#">Hold</a>
+      <ul>
+        <li><a href="#">Hold 1</a></li>
+        <li><a href="#">Hold 2</a></li>
+        <li><a href="#">Hold 3</a></li>
+        <li><a href="#">Hold 4</a></li>
+      </ul>
+    </li>
+  </ul>
+</nav>
+
+  </body>
+</html>
