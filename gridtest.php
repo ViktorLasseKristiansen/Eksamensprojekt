2513d057046526314f150e654660bddcb61fc443
Viktor Lasse <48516212+ViktorLasse@users.noreply.github.com>
2019-03-20 11:11:19 +0100

    Arbejdet på grid og gridstyle

--- gridtest.php
+++ gridtest.php
@@ -1,14 +1,51 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Gridwork</title>
-    <link rel="stylesheet" href="gridstyle.css">
-  </head>
-  <body>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+* {
+  box-sizing: border-box;
+}
 
+body {
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
 
-<!-- The grid: three columns -->
+/* The grid: Three equal columns that floats next to each other */
+.column {
+  float: left;
+  width: 33.33%;
+  padding: 50px;
+  text-align: center;
+  font-size: 25px;
+  cursor: pointer;
+  color: white;
+}
+
+.containerTab {
+  padding: 20px;
+  color: white;
+}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+/* Closable button inside the container tab */
+.closebtn {
+  float: right;
+  color: white;
+  font-size: 35px;
+  cursor: pointer;
+}
+</style>
+</head>
+<body>
+
 <div style="text-align:center">
   <h2>Expanding Grid</h2>
   <p>Click on the boxes below:</p>
@@ -26,6 +63,8 @@
     Box 3
   </div>
 </div>
+
+<!-- Full-width columns: (hidden by default) -->
 <div id="b1" class="containerTab" style="display:none;background:green">
   <span onclick="this.parentElement.style.display='none'" class="closebtn">&times;</span>
   <h2>Box 1</h2>
@@ -43,18 +82,17 @@
   <h2>Box 3</h2>
   <p>Lorem ipsum dolor sit amet, te quo doctus abhorreant, et pri deleniti intellegat, te sanctus inermis ullamcorper nam. Ius error diceret deseruisse ad</p>
 </div>
-<script type="text/javascript">
-// Hide all elements with class="containerTab", except for the one that matches the clickable grid column
+
+<script>
 function openTab(tabName) {
-var i, x;
-x = document.getElementsByClassName("containerTab");
-for (i = 0; i < x.length; i++) {
-  x[i].style.display = "none";
-}
-document.getElementById(tabName).style.display = "block";
+  var i, x;
+  x = document.getElementsByClassName("containerTab");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById(tabName).style.display = "block";
 }
 </script>
-
 
 </body>
 </html>
