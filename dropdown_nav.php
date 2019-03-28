b041b3c7dcdeb844e69f7f98a4bfaff541748647
Mayenco <haakansson.faxholm@gmail.com>
2019-03-28 17:42:12 +0100

    Opdateret dropdown navbar - venstre side
    Har opdateret navbaren, fylder 100% af venstre side, bruger java og fungere 95%. Vi snakker snarest muligt om følgene krav. Har fjernet css folderen til navbar, da den er ubrugelig med ny version. Og ja, den er baseret på en guide, ændre den efter samtale så den følger vores behov.

--- dropdown_nav.php
+++ dropdown_nav.php
@@ -1,36 +1,136 @@
 <!DOCTYPE html>
-<html lang="dk" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-    <link rel="stylesheet" type="text/css" href="style_nav.css">
-  </head>
-  <body>
-    <?php
-    if isset(9)
-     ?>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+body {
+  font-family: "Lato", sans-serif;
+}
 
-<nav>
-  <p>Navigation</p>
-  <ul>
-    <li><a href="#">Personer</a>
-      <ul>
-        <li><a href="dropdown_:nav.php?p_id=1">Person 1</a></li>
-        <li><a href="#">Person 2</a></li>
-        <li><a href="#">Person 3</a></li>
-        <li><a href="#">Person 4</a></li>
-      </ul>
-    </li>
-    <li><a href="#">Hold</a>
-      <ul>
-        <li><a href="#">Hold 1</a></li>
-        <li><a href="#">Hold 2</a></li>
-        <li><a href="#">Hold 3</a></li>
-        <li><a href="#">Hold 4</a></li>
-      </ul>
-    </li>
-  </ul>
-</nav>
+.sidenav {
+  height: 100%;
+  width: 250px;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  background-color: #111;
+  overflow-x: hidden;
+  padding-top: 20px;
+}
 
-  </body>
+.sidenav a, .dropdown-btn {
+  padding: 6px 8px 6px 16px;
+  text-decoration: none;
+  font-size: 20px;
+  color: #818181;
+  display: block;
+  border: none;
+  background: none;
+  width: 100%;
+  text-align: left;
+  cursor: pointer;
+  outline: none;
+}
+
+.sidenav a:hover, .dropdown-btn:hover {
+  color: #f1f1f1;
+}
+
+.main {
+  margin-left: 250px; /* Same as the width of the sidenav */
+  font-size: 20px; /* Increased text to enable scrolling */
+  padding: 0px 10px;
+}
+
+.active {
+  background-color: silver;
+  color: black;
+}
+
+.dropdown-container {
+  display: none;
+  background-color: #262626;
+  padding-left: 8px;
+}
+
+.fa-caret-down {
+  float: right;
+  padding-right: 8px;
+}
+
+@media screen and (max-height: 450px) {
+  .sidenav {padding-top: 15px;}
+  .sidenav a {font-size: 18px;}
+}
+</style>
+</head>
+<body>
+
+<div class="sidenav">
+  <button class="dropdown-btn">Personer
+    <i class="fa fa-caret-down"></i>
+      </button>
+        <div class="dropdown-container">
+          <button class="dropdown-btn">Anders
+            <i class="fa fa-caret-down"></i>
+              </button>
+                <div class="dropdown-container">
+                  <a href="#">Skema</a>
+                  <a href="#">Hold</a>
+                  <a href="#">Besked</a>
+                </div>
+          <button class="dropdown-btn">Viktor
+            <i class="fa fa-caret-down"></i>
+              </button>
+                <div class="dropdown-container">
+                  <a href="#">Skema</a>
+                  <a href="#">Hold</a>
+                  <a href="#">Besked</a>
+                </div>
+        </div>
+  <button class="dropdown-btn">Hold
+    <i class="fa fa-caret-down"></i>
+      </button>
+        <div class="dropdown-container">
+          <button class="dropdown-btn">Hold 1 - Planlægning
+            <i class="fa fa-caret-down"></i>
+              </button>
+                <div class="dropdown-container">
+                  <a href="#">Kontakt</a>
+                  <a href="#">Information</a>
+                </div>
+          <button class="dropdown-btn">Hold 2 - Udvikling
+            <i class="fa fa-caret-down"></i>
+              </button>
+                <div class="dropdown-container">
+                  <a href="#">Kontakt</a>
+                  <a href="#">Information</a>
+                </div>
+        </div>
+</div>
+
+<div class="main">
+  <!-- text -->
+</div>
+
+<script>
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+  this.classList.toggle("active");
+  var dropdownContent = this.nextElementSibling;
+  if (dropdownContent.style.display === "block") {
+  dropdownContent.style.display = "none";
+  } else {
+  dropdownContent.style.display = "block";
+  }
+  });
+}
+</script>
+
+</body>
 </html>
